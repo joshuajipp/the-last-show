4f9e54b8c7b3c2994621cf821f69872be85983d9
joshuajipp <joshuajipp@gmail.com>
2023-04-18 15:37:41 -0600

    initial function create

--- functions/create-obituary/main.py
+++ functions/create-obituary/main.py
@@ -1,1 +1,17 @@
-# add your create-obituary function here
+import boto3
+
+
+def lambda_handler():
+    pass
+
+
+def create_mp3(text):
+    client = boto3.client('polly')
+    response = client.synthesize_speech(
+        Engine='standard',
+        LanguageCode='en-US',
+        OutputFormat='mp3',
+        Text=text,
+        TextType='text',
+        VoiceId='Joanna'
+    )
