9a9af890d59a459cc9f6c78b21564a9d4ddc2be2
mizybermas <mizy.bermas@ucalgary.ca>
2023-04-19 14:39:36 -0600

    added header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
+import React from "react";
+import Header from "./components/Header";
 function App() {
-  return <h1>The Last Show</h1>;
+  return(
+    <Header />
+  )
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+function Headers(){
+    return(
+        <nav>
+            <div className = "header">
+                <div className = "title">
+                    <h1>The Last Show </h1>
+                </div>
+            </div>
+            <div className = "menu-button">
+                <button className = "button">+ New Obituary</button>
+            </div>
+        </nav>
+    )
+}
+export default Headers;
