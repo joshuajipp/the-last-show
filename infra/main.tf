862d9b72909561a13de7a7e1b770312ef70b415e
joshuajipp <joshuajipp@gmail.com>
2023-04-21 17:38:08 -0600

    zipped artifacts to be deployed to aws lambda

--- infra/main.tf
+++ infra/main.tf
@@ -15,3 +15,17 @@ provider "aws" {
 # one dynamodb table
 # roles and policies as needed
 # step functions (if you're going for the bonus marks)
+
+data "archive_file" "lambda_create_obituary" {
+  type = "zip"
+
+  source_dir  = "../functions/create-obituary"
+  output_path = "./create-obituary.zip"
+}
+
+data "archive_file" "lambda_get_obituaries" {
+  type = "zip"
+
+  source_dir  = "../functions/get-obituaries"
+  output_path = "./get-obituaries.zip"
+} 
