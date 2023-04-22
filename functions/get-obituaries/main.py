2cdf38a7cb8af87237790957f4167e658b4526e5
joshuajipp <joshuajipp@gmail.com>
2023-04-22 14:14:29 -0600

    created lambda function to retrieve obituaries

--- functions/get-obituaries/main.py
+++ functions/get-obituaries/main.py
@@ -1,2 +1,20 @@
+import boto3
+
+
 def handler(event, context):
-    pass
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('the-last-show-30160521')
+
+    try:
+        response = table.scan()
+        items = response['Items']
+        return {
+            'statusCode': 200,
+            'body': items
+        }
+    except Exception as e:
+        print("Query failed: " + str(e))
+        return {
+            'statusCode': 500,
+            'body': str(e)
+        }
