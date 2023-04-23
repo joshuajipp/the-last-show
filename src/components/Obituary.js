3e52945eb3511d32008f664a6e488b85bcd61745
joshuajipp <joshuajipp@gmail.com>
2023-04-23 17:20:41 -0600

    added audio component to card

--- src/components/Obituary.js
+++ src/components/Obituary.js
@@ -1,16 +1,21 @@
-function Obituary(props){
-    return(
+import AudioPlayer from "./AudioPlayer";
+
+function Obituary(props) {
+  return (
     <div className="obituary">
-        <div className = "obituary-box">
-            <img src={props.image} alt={props.name} className="squared" />
-            <div className = "obituary-text">
-                <p>{props.name}</p>
-                <small>{props.birth}-{props.death}</small>
-                <p className ="content">{props.content}</p>
-            </div>
+      <div className="obituary-box">
+        <img src={props.image} alt={props.name} className="squared" />
+        <AudioPlayer audio={props.audio} />
+        <div className="obituary-text">
+          <p>{props.name}</p>
+          <small>
+            {props.birth}-{props.death}
+          </small>
+          <p className="content">{props.content}</p>
         </div>
+      </div>
     </div>
-    )
+  );
 }
 
 export default Obituary;
