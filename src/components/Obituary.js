a5e6cf613855eff021c6cd66f1687da8c25881a3
joshuajipp <joshuajipp@gmail.com>
2023-04-23 18:58:58 -0600

    added date formatting to obituary

--- src/components/Obituary.js
+++ src/components/Obituary.js
@@ -1,6 +1,38 @@
 import AudioPlayer from "./AudioPlayer";
 
 function Obituary(props) {
+  const dateBirthString = props.birth.replace(/-/g, "/");
+  const dateDeathString = props.death.replace(/-/g, "/");
+  const birthDate = new Date(dateBirthString);
+  const deathDate = new Date(dateDeathString);
+  console.log(birthDate);
+
+  const monthNames = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  const birthDay = birthDate.getDate();
+  const birthMonthIndex = birthDate.getMonth();
+  const birthYear = birthDate.getFullYear();
+
+  const deathDay = deathDate.getDate();
+  const deathMonthIndex = deathDate.getMonth();
+  const deathYear = deathDate.getFullYear();
+
+  const formattedBirthDate = `${monthNames[birthMonthIndex]} ${birthDay}, ${birthYear}`;
+  const formattedDeathDate = `${monthNames[deathMonthIndex]} ${deathDay}, ${deathYear}`;
+
   return (
     <div className="obituary">
       <div className="obituary-box">
@@ -9,7 +41,7 @@ function Obituary(props) {
         <div className="obituary-text">
           <p>{props.name}</p>
           <small>
-            {props.birth}-{props.death}
+            {formattedBirthDate} - {formattedDeathDate}
           </small>
           <p className="content">{props.content}</p>
         </div>
