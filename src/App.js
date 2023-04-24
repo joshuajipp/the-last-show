36b4a6838493044ce3e4aef62e5598efef2eb04b
Joshua Jipp <102313118+joshuajipp@users.noreply.github.com>
2023-04-24 14:15:16 -0600

    added elements for grid format

--- src/App.js
+++ src/App.js
@@ -2,13 +2,12 @@ import React, { useEffect } from "react";
 import Header from "./components/Header";
 import Obituary from "./components/Obituary";
 import AddObituaryModal from "./components/AddObituaryModal";
-import {useState} from "react";
+import { useState } from "react";
 
 function App() {
-
   const [obituaries, setObituaries] = useState([]);
-  const [showModal,setShowModal] = useState(false);
-  
+  const [showModal, setShowModal] = useState(false);
+
   function openModal() {
     setShowModal(true);
   }
@@ -16,55 +15,69 @@ function App() {
     setShowModal(false);
   }
 
-  async function newObituary(newObituary){
-    const res = await fetch("https://yjru7bpx6g7uk6utxa76m526re0gmwce.lambda-url.ca-central-1.on.aws/",
-    {
-      method: "POST",
-      headers:{"Content-Type":"application/json"},
-      body: JSON.stringify(newObituary)
-    });
+  async function newObituary(newObituary) {
+    const res = await fetch(
+      "https://yjru7bpx6g7uk6utxa76m526re0gmwce.lambda-url.ca-central-1.on.aws/",
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(newObituary),
+      }
+    );
     getObituary();
   }
 
-  async function getObituary(){
-    const res = await fetch ("https://jbuz7pyvhj4xta57aexulh3xry0dncjp.lambda-url.ca-central-1.on.aws/",
-    {
-      method: "GET",
-      headers: {"Content-Type":"application/json"},
-    })
+  async function getObituary() {
+    const res = await fetch(
+      "https://jbuz7pyvhj4xta57aexulh3xry0dncjp.lambda-url.ca-central-1.on.aws/",
+      {
+        method: "GET",
+        headers: { "Content-Type": "application/json" },
+      }
+    );
     const data = await res.json();
-  
+
     setObituaries(data);
     closeModal();
   }
-  useEffect(()=> {
+  useEffect(() => {
     getObituary();
-  },[]);
-  
-  return(
-    <div>
-      <AddObituaryModal onNew ={newObituary} showModal={showModal} setShowModal= {setShowModal} closeModal={closeModal}/>
-      <div className = "home-page">
-      <Header toggleModal={openModal} />
-      {obituaries.length > 0 ? obituaries.map((obituaryItem) => 
-        <Obituary
-          image = {obituaryItem.image_url}
-          name = {obituaryItem.name}
-          birth = {obituaryItem.birth_date}
-          death = {obituaryItem.death_date}
-          content = {obituaryItem.text}
-          audio = {obituaryItem.mp3_url}
-        />
-      ) : (
-        <div className ="no-obituary">
-          <p className = "no-obituary-text">No Obituaries Yet</p>
-        </div>
-      )}
+  }, []);
 
+  return (
+    <div>
+      <AddObituaryModal
+        onNew={newObituary}
+        showModal={showModal}
+        setShowModal={setShowModal}
+        closeModal={closeModal}
+      />
+      <div className="home-page">
+        <Header toggleModal={openModal} />
+
+        {obituaries.length > 0 ? (
+          <div className="obituary-container">
+            {obituaries.map((obituaryItem) => (
+              <div className="card-parent">
+                <Obituary
+                  image={obituaryItem.image_url}
+                  name={obituaryItem.name}
+                  birth={obituaryItem.birth_date}
+                  death={obituaryItem.death_date}
+                  content={obituaryItem.text}
+                  audio={obituaryItem.mp3_url}
+                />
+              </div>
+            ))}
+          </div>
+        ) : (
+          <div className="no-obituary">
+            <p className="no-obituary-text">No Obituaries Yet</p>
+          </div>
+        )}
       </div>
     </div>
-    
-  )
+  );
 }
 
 export default App;
